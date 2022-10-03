dbcc8262c936d7491c9be538e621b1025d5c655e
Juuso Takalainen <juuso@streamr.network>
2022-10-03 15:19:21 +0300

    refactor: move "old format" scripts to old/
    to make it extra clear to use the "format2" stuff, not the "old format" stuff

--- src/old/distribute-format1.js
+++ src/old/distribute-format1.js
@@ -1,82 +1,78 @@
-// format2: address,reward with no quotes or anything like that
-// e.g.: 0x0001D577750221C08bEF4A908833f855eAf27243,39.5038420844966
+// "old format": "index","address",reward i.e. index and address are quoted, reward is not
+// e.g.: "1","0x00000a9A7c4Afe3DB554a2df54e9a0ec9d485080",0.37808553932401
 
-// also no state file (for smaller batches, manual restart/recovery using START index variable)
-// TODO: re-add stipend check!
-
-import { readFile } from "fs/promises"
-import { providers, Contract, Wallet, BigNumber } from "ethers"
-import { getAddress, parseEther, formatEther, parseUnits } from "ethers/lib/utils"
-
-const { JsonRpcProvider } = providers
+const fs = require("fs/promises")
+const {
+    providers: { JsonRpcProvider },
+    Wallet,
+    Contract,
+    utils: { getAddress, parseEther, formatEther }
+} = require("ethers")
 
 const {
-    INPUT,
-    START = "0",
+    INPUT = "data/2021-11-22-testnet.csv",
+    START = "",
     END = "Infinity",
     BATCH_SIZE = "100",
     SLEEP_MS = "1000",
-    GASPRICE_GWEI = "100",
-    ETHEREUM_URL = "https://polygon-rpc.com",
+    ETHEREUM_URL = "http://localhost:8545",
+    STATE_FILE_NAME = "last_rewarded_index.txt",
     KEY,
-    ADDRESS = "0x3979f7d6b5c5bfa4bcd441b4f35bfa0731ccfaef",
+    ADDRESS,
 } = process.env
 
 if (!KEY) { throw new Error("KEY environment variable is required for signing `send` transactions") }
 if (!ADDRESS) { throw new Error("ADDRESS environment variable is required: the Distributor contract address") }
-if (!INPUT) { throw new Error("INPUT environment variable is required: the rewards csv file") }
 
 const batchSize = +BATCH_SIZE
 const sleepMs = +SLEEP_MS
 
-const gasPrice = parseUnits(GASPRICE_GWEI, "gwei")
-
 const provider = new JsonRpcProvider(ETHEREUM_URL)
 const wallet = new Wallet(KEY, provider)
 
-const DistributorJson = require("../deployments/matic/Distributor.json")
+const DistributorJson = require("../../deployments/matic/Distributor.json")
 const contractAddress = getAddress(ADDRESS || DistributorJson.address)
 const distributor = new Contract(contractAddress, DistributorJson.abi, wallet)
 
-const TokenJson = require("../artifacts/contracts/TestToken.sol/TestToken.json")
+const TokenJson = require("../../artifacts/contracts/TestToken.sol/TestToken.json")
 
-async function sleep(ms: number) {
+async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-type Target = {
-    index: number
-    address: string
-    reward: BigNumber
-}
-
-async function sendRewards(targets: Target[]) {
+async function sendRewards(targets) {
     const first = targets[0]
     const last = targets[targets.length - 1]
     console.log("Sending indexes %s...%s, addresses %s...%s", first.index, last.index, first.address, last.address)
 
     const addresses = targets.map(({ address }) => address)
     const amounts = targets.map(({ reward }) => reward)
-    const tx = await distributor.send(addresses, amounts, { gasPrice })
+    const tx = await distributor.send(addresses, amounts)
     console.log("Sent tx: https://polygonscan.com/tx/%s", tx.hash)
     const tr = await tx.wait()
     console.log("Tx complete, gas spent: %s", tr.gasUsed.toString())
+
+    // persist the last rewarded index for automatic recovery
+    if (STATE_FILE_NAME) {
+        await fs.writeFile(STATE_FILE_NAME, last.index)
+    }
 }
 
 async function main() {
     console.log("Connected to network %o", await provider.getNetwork())
 
-    const rawInput = (await readFile(INPUT!, "utf8")).split("\n").slice(+START, +END)
+    const lastRewardedIndex = +await fs.readFile(STATE_FILE_NAME, "utf8").then(JSON.parse).catch(() => -1) // ignore missing file
+    const startIndex = START !== "" ? +START : lastRewardedIndex // csv file is 1-indexed so this actually works out fine
+    const rawInput = (await fs.readFile(INPUT, "utf8")).split("\n").slice(startIndex, END)
     let sum = parseEther("0")
-    const input = rawInput.map((line, index): Target => {
-        const [rawAddress, floatReward] = line.split(",")
-        // console.log("%s: %s, %s", index, rawAddress, floatReward)
-        const address = getAddress(rawAddress)
-        const reward = parseEther(floatReward.toString().slice(0, 20)) // remove decimals past 18th, otherwise parseEther throws
+    const input = rawInput.map(line => {
+        const [index, rawAddress, floatReward] = line.split(",")
+        const address = getAddress(rawAddress.slice(1, -1)) // remove quotes
+        const reward = parseEther(floatReward.toString())
         sum = sum.add(reward)
         return { index, address, reward }
-    }).filter(target => target.reward.gt(0)) // filter out zero reward lines
-    console.log("Starting from index %d, %d in total, distributing %s tokens in total", START, input.length, formatEther(sum))
+    })
+    console.log("Starting from index %d, %d in total, distributing %s tokens in total", startIndex, input.length, formatEther(sum))
 
     const tokenAddress = await distributor.token()
     const token = new Contract(tokenAddress, TokenJson.abi, wallet)
